15e2053019b31e0a4d24c242481132ddb009444c
Guillermo <guilleballarin73@gmail.com>
2024-05-08 22:25:46 +0200

    creacion subpaginas

---
+++ app/pages/centre/presentació/page.js
@@ -0,0 +1,11 @@
+import NavBar from '@/components/NavBar'
+
+export default function Home () {
+  return (
+    <>
+      <NavBar />
+      <h1>Presentació</h1>
+      <h2>Lo que sea</h2>
+    </>
+  )
+}
