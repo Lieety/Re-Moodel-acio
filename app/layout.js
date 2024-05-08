beecbccb27b10103d2f5805eb38f260a8762a8d5
Guillermo <guilleballarin73@gmail.com>
2024-05-08 22:25:05 +0200

    arreglar head

--- app/layout.js
+++ app/layout.js
@@ -12,13 +12,7 @@ export const metadata = {
 export default function RootLayout ({ children }) {
   return (
     <html lang='es' className='flex flex-col-reverse'>
-      <body className={inter.className}>
-<<<<<<< HEAD
-=======
-        <NavBar />
->>>>>>> 3320725774b19cf8eb6b5eca0ccc816fa6457acc
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
