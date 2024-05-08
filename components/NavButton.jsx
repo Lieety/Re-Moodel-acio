98b7c3c1b193e3888c9faff67b60360388be118e
Guillermo <guilleballarin73@gmail.com>
2024-05-08 22:26:28 +0200

    creacion de links automatico

--- components/NavButton.jsx
+++ components/NavButton.jsx
@@ -3,6 +3,18 @@ import { Router } from "next/router"
 
 
 export default function NavButton ({ label, opt }) {
+  function linkMaker(text){
+    text = text.toLowerCase()
+    let separados = text.split(' '||"'")
+    let textoFinal = ''
+    for(let e in separados){
+      let length = textoFinal.length
+      let conjunto = (length!=0)?'_':''
+      textoFinal = textoFinal + conjunto + separados[e]
+    }
+    return textoFinal
+    }
+  
   return (
       
       <li  className="p-4 lg:px-8 group relative flex items-center space-x-1 ">
@@ -12,7 +24,8 @@ export default function NavButton ({ label, opt }) {
               {opt.map(e => {
                 return(
                   <li key={e}>
-                    <a className="text-slate-800 hover:bg-slate-50 flex items-center p-2" href="#">
+                    
+                    <a className="text-slate-800 hover:bg-slate-50 flex items-center p-2" href={"/pages/"+label.toLowerCase()+'/'+linkMaker(e)}>
                         <span className="whitespace-nowrap">{e}</span>
                     </a>
                 </li>
