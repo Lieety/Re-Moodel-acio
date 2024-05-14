e8411bd80d641e71dcdadf199f8532b1d8edfb06
Lieety <lieety10.07@gmail.com>
2024-05-14 20:15:20 +0200

    asdfghjklñ

--- src/components/log/Register.jsx
+++ src/components/log/Register.jsx
@@ -1,9 +1,20 @@
 'use client'
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import React, { useState } from 'react'
 import './style.css'
+import { FaRegEyeSlash, FaRegEye, FaFacebook, FaGoogle } from "react-icons/fa";
 
 export default function Register () {
+  const [showPass, setShowPass] = useState(false);
+  const [password, setPassword] = useState('');
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const togglePasswordVisibility = (e) => {
+    e.preventDefault()
+    setShowPass(!showPass);
+  };
   return (
     <div className='form-container sign-up-container'>
       <form action="" className=''>
@@ -15,7 +26,28 @@ export default function Register () {
         <span>o utiliza tu mail</span>
         <input type="text" placeholder='nombre' required />
         <input type="email" placeholder='e-mail' required />
-        <input type="password" placeholder='contraseña' required />
+        <div className="password-container input" style={{ width: '100%' }}>
+          <input
+            type={showPass ? 'text' : 'password'}
+            placeholder='contraseña'
+            value={password}
+            id='password'
+            onChange={handlePasswordChange}
+            required
+            style={{ width: '100%' }}
+          />
+          <span className="show-password-checkbox">
+            <button
+              id="showPassword"
+              onClick={togglePasswordVisibility}
+            >
+              <label htmlFor="showPassword">
+              {showPass ? <FaRegEyeSlash /> : <FaRegEye />}
+            </label>
+            </button>
+            
+          </span>
+        </div>
         <select name="role" id="role" >
           <option value="Profesor">Profesor/a</option>
           <option value="Alumno">Alumno/a</option>
