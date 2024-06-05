f76f3d5d892df7ab2420275d9a1f071cc9fb7c60
Guillermo <guilleballarin73@gmail.com>
2024-06-05 20:06:32 +0200

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,3 @@
 'use client'
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
-
-export const Home = () => {
-  const { push } = useRouter()
-
-  useEffect(() => {
-    push('/main')
-  }, [])
-  return <p></p>
-}
